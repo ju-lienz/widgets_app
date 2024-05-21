929d20d627db011f8ffd079eb1cef362989f0894
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-05-21 08:48:45 -0300

    feat: create app_theme.dart with application themes and implement in main.dart

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+/// This list stores all the colors used in the app
+const colorList = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.pink,
+  Colors.orange,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  /// The currently selected color index
+  final int selectedColor;
+
+  // Constructor with an optional parameter for selectedColor
+  // Defaults to the first color (index 0)
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'Selected colores must be greater than 0'),
+        assert(selectedColor < colorList.length,
+            'Selected colores must be less or equal than ${colorList.length - 1}');
+
+  /// Creates a ThemeData object based on the selected color
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        // Sets the primary color based on the selected color index in the list
+        colorSchemeSeed: colorList[selectedColor],
+      );
+}
