9c9c5500c248bc06d02211391ef2371c2265a8c3
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-05-30 11:33:41 -0300

    add dialogs

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -20,6 +21,29 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  //BuildContext is the info of the app
+  void openDialog(BuildContext context) {
+    showDialog(
+      barrierDismissible: false, // Dialog requires button press to close
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Estás seguro?"),
+        content: const Text(
+            "Do deserunt veniam mollit enim occaecat laboris exercitation dolor ut duis duis commodo ea ullamco. Dolore mollit mollit fugiat enim incididunt veniam excepteur velit elit deserunt. Quis ullamco duis dolore sunt do irure."),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text("Cancelar"),
+          ),
+          FilledButton(
+            onPressed: () => context.pop(),
+            child: const Text("Aceptar"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +54,29 @@ class SnackbarScreen extends StatelessWidget {
         label: const Text("Mostrar Snackbar"),
         icon: const Icon(Icons.remove_red_eye_outlined),
         onPressed: () => showCustomSnackbar(context),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text(
+                        "Nostrud non ipsum culpa duis ullamco occaecat magna. Deserunt et commodo nisi laborum. Consequat pariatur exercitation aliqua aliqua laboris deserunt nulla incididunt voluptate anim velit. Quis in cupidatat eiusmod aliqua eiusmod occaecat tempor culpa nisi nisi voluptate adipisicing. Exercitation eiusmod nulla laborum reprehenderit aute magna velit voluptate excepteur anim ipsum in. Non est ex magna ullamco ut ipsum culpa.")
+                  ],
+                );
+              },
+              child: const Text('Licencias usadas'),
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar dialog'),
+            ),
+          ],
+        ),
       ),
     );
   }
