70bd80c9438d91a7111d35f6c21afe53d22819e7
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-05-31 10:32:23 -0300

    finish ui screen

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -28,6 +28,9 @@ enum Transportation { car, plane, boat, submarine }
 class _UIControlsViewState extends State<_UIControlsView> {
   bool isDeveloper = false;
   Transportation selectedTransportation = Transportation.car;
+  bool wantsBrakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -41,44 +44,74 @@ class _UIControlsViewState extends State<_UIControlsView> {
             isDeveloper = !isDeveloper;
           }),
         ),
-        RadioListTile(
-          title: const Text('By car'),
-          subtitle: const Text('Viajar por carro'),
-          value:
-              Transportation.car, // Enlazar valor seleccionado con valor actual
-          groupValue: selectedTransportation, // Current selected option
+        ExpansionTile(
+          initiallyExpanded: true,
+          title: const Text("Vehiculo de transporte"),
+          subtitle: Text('$selectedTransportation'),
+          children: [
+            //A group of radio checks
+            RadioListTile(
+              title: const Text('By car'),
+              subtitle: const Text('Viajar por carro'),
+              value: Transportation
+                  .car, // Enlazar valor seleccionado con valor actual
+              groupValue: selectedTransportation, // Current selected option
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.car;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By boat'),
+              subtitle: const Text('Viajar por bote'),
+              value: Transportation
+                  .boat, // Enlazar valor seleccionado con valor actual
+              groupValue: selectedTransportation, // Current selected option
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.boat;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By plane'),
+              subtitle: const Text('Viajar por avion'),
+              value: Transportation
+                  .plane, // Enlazar valor seleccionado con valor actual
+              groupValue: selectedTransportation, // Current selected option
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.plane;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By submarine'),
+              subtitle: const Text('Viajar por submarino'),
+              value: Transportation
+                  .submarine, // Enlazar valor seleccionado con valor actual
+              groupValue: selectedTransportation, // Current selected option
+              onChanged: (value) => setState(() {
+                selectedTransportation = Transportation.submarine;
+              }),
+            ),
+          ],
+        ),
+        //An individual checkbox
+        CheckboxListTile(
+          title: const Text("Incluir desayuno:"),
+          value: wantsBrakfast,
           onChanged: (value) => setState(() {
-            selectedTransportation = Transportation.car;
+            wantsBrakfast = !wantsBrakfast;
           }),
         ),
-        RadioListTile(
-          title: const Text('By boat'),
-          subtitle: const Text('Viajar por bote'),
-          value: Transportation
-              .boat, // Enlazar valor seleccionado con valor actual
-          groupValue: selectedTransportation, // Current selected option
+        CheckboxListTile(
+          title: const Text("Incluir almuerzo:"),
+          value: wantsLunch,
           onChanged: (value) => setState(() {
-            selectedTransportation = Transportation.boat;
+            wantsLunch = !wantsLunch;
           }),
         ),
-        RadioListTile(
-          title: const Text('By plane'),
-          subtitle: const Text('Viajar por avion'),
-          value: Transportation
-              .plane, // Enlazar valor seleccionado con valor actual
-          groupValue: selectedTransportation, // Current selected option
+        CheckboxListTile(
+          title: const Text("Incluir cena:"),
+          value: wantsDinner,
           onChanged: (value) => setState(() {
-            selectedTransportation = Transportation.plane;
-          }),
-        ),
-        RadioListTile(
-          title: const Text('By submarine'),
-          subtitle: const Text('Viajar por submarino'),
-          value: Transportation
-              .submarine, // Enlazar valor seleccionado con valor actual
-          groupValue: selectedTransportation, // Current selected option
-          onChanged: (value) => setState(() {
-            selectedTransportation = Transportation.submarine;
+            wantsDinner = !wantsDinner;
           }),
         ),
       ],
