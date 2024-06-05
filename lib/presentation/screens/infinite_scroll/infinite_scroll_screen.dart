a6daebe8abe0dbb991373ab81dc1861c4db36458
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-06-05 08:17:43 -0300

    add loading indicator

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -41,9 +42,9 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     );
     addFiveImages();
     isLoading = false;
-    //TODO: iOS
     if (isMounted) return;
     setState(() {});
+    //TODO: Move scroll
   }
 
   void addFiveImages() {
@@ -79,7 +80,14 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back),
+        child: isLoading
+            ? SpinPerfect(
+                infinite: true,
+                child: const Icon(Icons.refresh),
+              )
+            : FadeIn(
+                child: const Icon(Icons.arrow_back),
+              ),
       ),
     );
   }
