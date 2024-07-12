d1de018813274acacd882b784e0d02a24df92bca
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-07-12 08:40:38 -0300

    add counter provider

---
+++ lib/presentation/providers/counter_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Each provider stores a specific state, promoting modularity and separation of concerns. This makes
+/// providers easier to maintain, test, and reuse.
+///
+/// A `stateProvider` is a small piece of state information that can be used to manage the
+/// state of various aspects of the application. For example, it can be used to track the
+/// selected theme, the state of a form, or the data loaded from an API.
+///
+/// Here, the `counterProvider` is used to track the value of a counter. The initial value is 5,
+/// and the counter can be incremented or decremented using provider actions.
+final counterProvider = StateProvider((ref) => 5);
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+/// A `ConsumerWidget` widget that aims to access a builder and obtain the values
+/// I need from my provider.
+///
+/// It provides a `WidgetRef ref` reference in the `build` method. It's a way to
+/// notify Riverpod that a reference to a provider will be used, which we will specify later.
+class CounterScreen extends ConsumerWidget {
   // Name of the route for go_router config
   static const String name = 'counter';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    /// The current value of the counter, retrieved from the `counterProvider`.
+    /// The `Watch` method allows you to listen for changes in state.
+    final int clickCounter = ref.watch(counterProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Counter Riverpod"),
       ),
-      body: const Center(
+      body: Center(
         child: Text(
-          "Valor: 10",
-          style: TextStyle(fontSize: 28),
+          "Valor:$clickCounter",
+          style: const TextStyle(fontSize: 28),
         ),
       ),
       floatingActionButton: FloatingActionButton(
